a31e845e60db8b0025895942bce582f1b52cc9e4
burt23 <mail4mdb@gmail.com>
2017-04-07 15:27:14 -0700

    (feat) connected barebones express server to react client

--- server/server.js
+++ server/server.js
@@ -2,11 +2,16 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
-const port = process.env.PORT || 8888;
+const port = process.env.PORT || 8000;
 const app = express();
 
-app.use('/', express.static(path.static(__.dirname + '../src/client')));
+app.use('/', express.static(path.join(__dirname, '../src/client')));
+
+app.get('/home', function(req, res){
+  res.send('hello world from home');
+})
 
 app.listen(port, function(){
   console.log('Express Service live and listening on: ', port);
