c78828cd76644ec9da2dfadb231bdd59364eb7b0
samzoll22 <sam.zoll22@gmail.com>
2017-04-07 17:14:15 -0700

    (bug) fixed lint errors and cleaned up index.jsx/.html/.css

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -9,7 +9,7 @@ class App extends React.Component{
 
   render(){
     return(
-      <h1>Hello World</h1>
+      <h1 style={{'text-align':'center', 'font-size':'12em', 'color':'#ffffff'}}>Hello World</h1>
     )
   }
 }
