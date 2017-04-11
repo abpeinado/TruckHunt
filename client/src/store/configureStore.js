c5f4ed7cc06a28c0cd2c163b8b19495933330253
Matt Schmitz <mattschmitz9@gmail.com>
2017-04-10 22:05:15 -0700

    (bug) remove redux-devtools-extension for heroku

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -1,13 +1,25 @@
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import reducers from '../reducers';
 
 const configureStore = initialState => createStore(
   reducers,
   initialState,
-  composeWithDevTools(
   applyMiddleware(thunk),
-));
+);
 
 export default configureStore;
+
+// import { createStore, applyMiddleware } from 'redux';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+// import thunk from 'redux-thunk';
+// import reducers from '../reducers';
+
+// const configureStore = initialState => createStore(
+//   reducers,
+//   initialState,
+//   composeWithDevTools(
+//   applyMiddleware(thunk),
+// ));
+
+// export default configureStore;
