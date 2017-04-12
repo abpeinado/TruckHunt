eea76b42968f692cb46959ba4b269a4b8e9298c5
burt23 <mail4mdb@gmail.com>
2017-04-11 18:16:59 -0700

    Housekeeping

--- __test__/TruckList.test.js
+++ __test__/TruckList.test.js
@@ -1,24 +1,16 @@
 import { shallow } from 'enzyme';
 import React from 'react';
-// import renderer from 'react-test-renderer';
 import TruckList from '../client/src/components/TruckList';
-
-const sum = (a, b) => (a + b); // example function (import ./server/sum.js)
-
 
 describe('App Components Test', () => {
 
   test('find trucklistclass', () => {
     const TruckListWrapper = shallow(<TruckList />);
-    console.log('WRAPPER---------------------->', TruckListWrapper.find('TruckListWrapper.node.props.className').exists());
-
-
     expect(TruckListWrapper.find('TruckListWrapper.node.props.className').exists()).toEqual(false);
   });
 
   test('list has data to map', () => {
     const TruckListWrapper = shallow(<TruckList />);
-    console.log('WRAPPER---------------------->', TruckListWrapper.find('TruckListWrapper.node.props.className').exists());
     expect(TruckListWrapper.find('TruckListWrapper.node.props.className').exists()).toEqual(false);
   });
 });
