c2c0b77f480a15aa6bac01823790beb3f0ebcba0
Matt Schmitz <mattschmitz9@gmail.com>
2017-04-11 21:23:03 -0700

    (feat) build truck info page with hardcoded data
    - (feat) set up components for TruckInfo page
    
    - (feat) add route to app for truckinfo
    
    - (feat) link together mock truck info components
    
    - (feat) update css, move menu items into panels
    
    - (bug) fix bootstrap grid for truck info page
    
    - (feat) improve styling of trunk info summary
    
    - (feat) update cart title
    
    - (bug) remove comment from styles
    
    - (feat) change app back to display main page
    
    (bug) fix linter errors
    
    (refact) truck summery to use props, pass in fake data
    
    (feat) update truckInfoSummary for redux
    
    (feat) implement truck info redux actions
    
    (feat) pull data from server for truck summary

--- client/src/components/TruckInfoSummary.jsx
+++ client/src/components/TruckInfoSummary.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
+<<<<<<< HEAD
+=======
+import { connect } from 'react-redux';
+import { truckInfoFetchData } from '../actions/truckInfoActions.js';
+>>>>>>> (feat) build truck info page with hardcoded data
 
 class TruckInfoSummary extends React.Component {
   constructor(props) {
@@ -7,6 +12,7 @@ class TruckInfoSummary extends React.Component {
     this.state = {};
   }
 
+<<<<<<< HEAD
   render() {
     return (
       <Row className="truck-info-summary">
@@ -19,10 +25,45 @@ class TruckInfoSummary extends React.Component {
         </Col>
         <Col md={3} className="truck-rating">
           <h3> 3 stars </h3>
+=======
+  componentDidMount() {
+    this.props.fetchTruckInfo('');
+  }
+
+  render() {
+    const { image, name, description, rating } = this.props.truckSummary;
+    return (
+      <Row className="truck-info-summary">
+        <Col md={3}>
+          <img src={image} style={{ maxHeight: '90px' }} alt="Truck Icon" />
+        </Col>
+        <Col md={6}>
+          <h2> {name} </h2>
+          <p> {description} </p>
+        </Col>
+        <Col md={3} className="truck-rating">
+          <h3> {rating} stars </h3>
+>>>>>>> (feat) build truck info page with hardcoded data
         </Col>
       </Row>
     );
   }
 }
 
+<<<<<<< HEAD
 export default TruckInfoSummary;
+=======
+const mapStateToProps = (state) => {
+  return {
+    truckSummary: state.truckInfo
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchTruckInfo: (truckID) => dispatch(truckInfoFetchData(truckID))
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TruckInfoSummary);
+>>>>>>> (feat) build truck info page with hardcoded data
