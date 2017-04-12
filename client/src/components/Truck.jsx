27a527c7d27362d68a1bedb3bbd5162542f0001a
Matt Schmitz <mattschmitz9@gmail.com>
2017-04-11 21:23:03 -0700

    (feat) set up components for TruckInfo page

--- client/src/components/Truck.jsx
+++
@@ -1,3 +0,0 @@
-// Page that a user sees when they click on a TruckListItem
-// will be composed of a number of components that I haven't yet outlined,
-// but will probably need a title, an image, and a menu at least
---
+++ client/src/components/TruckInfoLeftPane.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class TruckInfoLeftPane extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        Left Pane
+      </div>
+    );
+  }
+}
+
+export default TruckInfoLeftPane;
