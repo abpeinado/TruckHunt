9405730014998be254da3c957e66f65ef7d7c533
Benicio Peinado <BenicioPeinado@gmail.com>
2017-04-13 16:43:39 -0700

    (feat) set up testing for database

--- database/schema.js
+++ database/schema.js
@@ -1,12 +1,20 @@
-module.exports = (db) => {
-  return db.query('CREATE TABLE IF NOT EXISTS users(\
-    user_id SERIAL PRIMARY KEY,\
-    email VARCHAR(30) NOT NULL UNIQUE,\
-    first_name VARCHAR(20) NOT NULL,\
-    last_name VARCHAR(20) NOT NULL,\
-    password VARCHAR(200) NOT NULL,\
-    phone VARCHAR(20),\
-    created TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,\
-    modified TIMESTAMPTZ\
-    );');
+module.exports.createTables = (db) => {
+  return db.query('DROP TABLE IF EXISTS\
+    users\
+    ')
+  .then(() => {
+    return db.query('CREATE TABLE IF NOT EXISTS users(\
+      user_id SERIAL PRIMARY KEY,\
+      email VARCHAR(30) NOT NULL UNIQUE,\
+      first_name VARCHAR(20) NOT NULL,\
+      last_name VARCHAR(20) NOT NULL,\
+      password VARCHAR(200) NOT NULL,\
+      phone VARCHAR(20),\
+      created TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,\
+      modified TIMESTAMPTZ\
+      );');
+  })
+  .catch((error) => {
+    console.log('error: ', error);
+  });
 };
