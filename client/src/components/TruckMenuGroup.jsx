0255f392babcf2d48794b7670d15492b24ada4a4
Matt Schmitz <mattschmitz9@gmail.com>
2017-04-12 19:11:32 -0700

    (bug) fix linter errors

--- client/src/components/TruckMenuGroup.jsx
+++ client/src/components/TruckMenuGroup.jsx
@@ -2,25 +2,23 @@ import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 import TruckMenuItem from './TruckMenuItem.jsx';
 
-class TruckMenuGroup extends React.Component {
-  render() {
-    // category, menu items
-    return (
-      <Row className="truck-menu-group">
-        <Col md={12}>
-          <Row>
-            <h3> Pizza </h3>
-          </Row>
-          <Row>
-            <TruckMenuItem />
-            <TruckMenuItem />
-            <TruckMenuItem />
-            <TruckMenuItem />
-          </Row>
-        </Col>
-      </Row>
-    );
-  }
-}
+const TruckMenuGroup = () => {
+  // requires a category and menu items
+  return (
+    <Row className="truck-menu-group">
+      <Col md={12}>
+        <Row>
+          <h3> Pizza </h3>
+        </Row>
+        <Row>
+          <TruckMenuItem />
+          <TruckMenuItem />
+          <TruckMenuItem />
+          <TruckMenuItem />
+        </Row>
+      </Col>
+    </Row>
+  );
+};
 
 export default TruckMenuGroup;
