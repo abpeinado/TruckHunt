b7010788970dc69006b196559b47cc589a75d00f
Benicio Peinado <BenicioPeinado@gmail.com>
2017-04-13 17:24:18 -0700

    (feat) clean up linting issues

--- server/models/users.js
+++ server/models/users.js
@@ -13,10 +13,10 @@ module.exports.findByEmail = (email) => {
   return db.oneOrNone('SELECT * FROM users WHERE email = $1', [email]);
 };
 
-module.exports.update = (userId, column, updatedValue) => {
-  return db.query('UPDATE users SET $1~ = $2 WHERE user_id = $3 RETURNING *', [column, updatedValue, userId]);
+module.exports.update = (user_id, column, updatedValue) => {
+  return db.query('UPDATE users SET $1~ = $2 WHERE user_id = $3 RETURNING *', [column, updatedValue, user_id]);
 };
 
-module.exports.delete = (userId) => {
-  return db.query('DELETE FROM users WHERE user_id = $1', [userId]);
+module.exports.delete = (user_id) => {
+  return db.query('DELETE FROM users WHERE user_id = $1', [user_id]);
 };
