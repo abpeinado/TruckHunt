c84d1db08e8b53b4db503a2d8a80bed5c4879ed3
Benicio Peinado <BenicioPeinado@gmail.com>
2017-04-15 12:07:36 -0700

    Update schema.js

--- database/schema.js
+++ database/schema.js
@@ -4,29 +4,32 @@ module.exports = (db) => {
     ')
   .then(() => {
     return db.query('CREATE TABLE IF NOT EXISTS vendors(\
-      user_id SERIAL PRIMARY KEY,\
+      vendor_id SERIAL PRIMARY KEY,\
+      permit_number VARCHAR(20) NOT NULL,\
       email VARCHAR(30) NOT NULL UNIQUE,\
+      phone_number VARCHAR(20) NOT NULL,\
       first_name VARCHAR(20) NOT NULL,\
       last_name VARCHAR(20) NOT NULL,\
+      food_categories VARCHAR(100),\
       password VARCHAR(200) NOT NULL,\
-      created TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,\
-      modified TIMESTAMPTZ\
+      salt VARCHAR(40),\
+      created TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP\
       );');
   })
   .then(() => {
     return db.query('CREATE TABLE IF NOT EXISTS customers(\
-      _id SERIAL PRIMARY KEY,\
-      email VARCHAR(50),\
+      customer_id SERIAL PRIMARY KEY,\
+      email VARCHAR(50) NOT NULL UNIQUE,\
       stripe_id INT,\
       username VARCHAR(30) NOT NULL,\
       password VARCHAR(255) NOT NULL,\
-      salt VARCHAR(40) NOT NULL,\
+      salt VARCHAR(40),\
       created TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP\
       );');
   })
   .then(() => {
     return db.query('CREATE TABLE IF NOT EXISTS reviews(\
-      _id SERIAL PRIMARY KEY,\
+      review_id SERIAL PRIMARY KEY,\
       customer_id INT NOT NULL,\
       vendor_id INT NOT NULL,\
       rating SMALLINT NOT NULL,\
@@ -35,10 +38,11 @@ module.exports = (db) => {
   })
   .then(() => {
     return db.query('CREATE TABLE IF NOT EXISTS menu_items(\
-      _id SERIAL PRIMARY KEY,\
-      vendor_id INT NOT NULL,\
+      menu_item_id SERIAL PRIMARY KEY,\
+      vendor_id INT,\
       name VARCHAR(100) NOT NULL,\
-      type VARCHAR(100),\
+      course VARCHAR(100),\
+      food_categories VARCHAR(100),\
       price SMALLINT NOT NULL,\
       item_description VARCHAR(500),\
       item_img VARCHAR(200)\
@@ -46,24 +50,24 @@ module.exports = (db) => {
   })
   .then(() => {
     return db.query('CREATE TABLE IF NOT EXISTS orders(\
-      _id SERIAL PRIMARY KEY,\
+      order_id SERIAL PRIMARY KEY,\
       vendor_id INT NOT NULL,\
       customer_id INT NOT NULL,\
-      menu_item_id INT NOT NULL,\
+      menu_item_id INT,\
       price_total INT NOT NULL,\
-      order_status SMALLINT NOT NULL,\
-      order_pickup SMALLINT NOT NULL,\
+      order_status SMALLINT,\
+      order_pickup SMALLINT,\
       order_time TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP\
       );');
   })
   .then(() => {
     return db.query('CREATE TABLE IF NOT EXISTS schedules(\
-      _id SERIAL PRIMARY KEY,\
-      time TIMESTAMPTZ NOT NULL,\
-      lat VARCHAR(255) NOT NULL,\
-      long VARCHAR(255) NOT NULL,\
-      vendor_id INT NOT NULL,\
-      day VARCHAR(50) NOT NULL\
+      schedule_id SERIAL PRIMARY KEY,\
+      start_time VARCHAR(20) NOT NULL,\
+      end_time VARCHAR(20) NOT NULL,\
+      day_of_week INT NOT NULL,\
+      coordinates VARCHAR(100) NOT NULL,\
+      vendor_id INT NOT NULL\
       );');
   })
   .catch((error) => {
