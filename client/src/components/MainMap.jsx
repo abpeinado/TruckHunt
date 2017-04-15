9f18722071c347c764cc34627341534e560cf96c
samzoll22 <sam.zoll22@gmail.com>
2017-04-15 14:51:30 -0700

    (feat) Implemented Mapbox into Consumer Homepage

--- client/src/components/MainMap.jsx
+++ client/src/components/MainMap.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
-import { Col } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
+import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
 
 export default function Map() {
   return (
-    <Col md={4}>
-      <div className="mainMap" >
-        <iframe
-          src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAXLEBPlNpPG_7Cx6WdXEszZ6woRdwQdnw
-            &q=Hack+Reactor,San+Francisco+CA"
-        />
-      </div>
-    </Col>
+    <Row >
+      <Col xs={12} md={8} >
+        <div className="mainMap" >
+          <ReactMapboxGl
+            style="mapbox://styles/mapbox/streets-v8"
+            accessToken="pk.eyJ1Ijoic3pvbGwiLCJhIjoiY2oxanIwcHI4MDFicDMzcG1wenNmbXlqbCJ9.oRYXUrA-6QSwq2tYGLN2xw"
+            containerStyle={{
+              height: "30vh",
+              width: "100vw"
+            }}
+            center={[-122.408966, 37.783697]}
+            zoom={[13]}
+            >
+              <Layer
+                type="symbol"
+                id="marker"
+                layout={{ "icon-image": "marker-15" }}>
+                <Feature coordinates={[37.784118, -122.406435]}/>
+              </Layer>
+          </ReactMapboxGl>
+        </div>
+      </Col>
+    </Row>
   );
 }
