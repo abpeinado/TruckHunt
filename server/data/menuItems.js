cd2ce02269647b8cdc747247b329785eabf9c14a
burt23 <mail4mdb@gmail.com>
2017-04-16 20:25:54 -0700

    Created sample menu items for generic food categories and updated schema

---
+++ server/data/menuItems.js
@@ -0,0 +1,229 @@
+module.exports.menuItems = {
+  'Burgers: melts: hot dogs: burritos:sandwiches: fries: onion rings: drinks':
+  [
+    {
+      name: 'French Fries',
+      description: 'yummy homemade fries',
+      price: 1.99,
+      title: 'Appetizers'
+    },
+    {
+      name: 'Hummus',
+      description: 'served with pita bread',
+      price: 4.99,
+      title: 'Appetizers'
+    },
+    {
+      name: 'Chicken Wings',
+      description: 'with celery sticks and blue cheese sauce',
+      price: 5.99,
+      title: 'Appetizers'
+    },
+    {
+      name: 'Onion Rings',
+      description: 'beer battered and served with chipotle mayo',
+      price: 4.99,
+      title: 'Appetizers'
+    },
+
+    {
+      name: 'Cesear',
+      description: 'yummy homemade fries',
+      price: 1.99,
+      title: 'Salads'
+    },
+    {
+      name: 'Potato Salad',
+      description: 'served with pita bread',
+      price: 4.99,
+      title: 'Salads'
+    },
+    {
+      name: 'Mexican',
+      description: 'with celery sticks and blue cheese sauce',
+      price: 5.99,
+      title: 'Salads'
+    },
+    {
+      name: 'Magical',
+      description: 'beer battered and served with chipotle mayo',
+      price: 4.99,
+      title: 'Salads'
+    },
+    {
+      name: 'Beef',
+      description: 'yummy homemade fries',
+      price: 1.99,
+      title: 'Tacos'
+    },
+    {
+      name: 'Chicken',
+      description: 'served with pita bread',
+      price: 4.99,
+      title: 'Tacos'
+    },
+    {
+      name: 'Lengua',
+      description: 'with celery sticks and blue cheese sauce',
+      price: 5.99,
+      title: 'Tacos'
+    },
+    {
+      name: 'Carnitas',
+      description: 'beer battered and served with chipotle mayo',
+      price: 4.99,
+      title: 'Tacos'
+    }
+  ],
+
+'Cold Truck: packaged sandwiches: snacks: candy: hot and cold drinks':
+  [
+    {
+      name: 'French Fries',
+      description: 'yummy homemade fries',
+      price: 1.99,
+      title: 'Appetizers'
+    },
+    {
+      name: 'Hummus',
+      description: 'served with pita bread',
+      price: 4.99,
+      title: 'Appetizers'
+    },
+    {
+      name: 'Chicken Wings',
+      description: 'with celery sticks and blue cheese sauce',
+      price: 5.99,
+      title: 'Appetizers'
+    },
+    {
+      name: 'Onion Rings',
+      description: 'beer battered and served with chipotle mayo',
+      price: 4.99,
+      title: 'Appetizers'
+    },
+    {
+      name: 'Cesear',
+      description: 'yummy homemade fries',
+      price: 1.99,
+      title: 'Salads'
+    },
+    {
+      name: 'Potato Salad',
+      description: 'served with pita bread',
+      price: 4.99,
+      title: 'Salads'
+    },
+    {
+      name: 'Mexican',
+      description: 'with celery sticks and blue cheese sauce',
+      price: 5.99,
+      title: 'Salads'
+    },
+    {
+      name: 'Magical',
+      description: 'beer battered and served with chipotle mayo',
+      price: 4.99,
+      title: 'Salads'
+    },
+    {
+      name: 'Beef',
+      description: 'yummy homemade fries',
+      price: 1.99,
+      title: 'Tacos'
+    },
+    {
+      name: 'Chicken',
+      description: 'served with pita bread',
+      price: 4.99,
+      title: 'Tacos'
+    },
+    {
+      name: 'Lengua',
+      description: 'with celery sticks and blue cheese sauce',
+      price: 5.99,
+      title: 'Tacos'
+    },
+    {
+      name: 'Carnitas',
+      description: 'beer battered and served with chipotle mayo',
+      price: 4.99,
+      title: 'Tacos'
+    }
+],
+  'Hot dogs: condiments: soft pretzels: soft drinks: coffee: cold beverages: pastries: bakery goods: cookies: ice cream: candy: soups: churros: chestnuts: nuts: fresh fruit: fruit juices: desserts: potato chips and popcorn.':
+  [
+    {
+      name: 'French Fries',
+      description: 'yummy homemade fries',
+      price: 1.99,
+      title: 'Appetizers'
+    },
+    {
+      name: 'Hummus',
+      description: 'served with pita bread',
+      price: 4.99,
+      title: 'Appetizers'
+    },
+    {
+      name: 'Chicken Wings',
+      description: 'with celery sticks and blue cheese sauce',
+      price: 5.99,
+      title: 'Appetizers'
+    },
+    {
+      name: 'Onion Rings',
+      description: 'beer battered and served with chipotle mayo',
+      price: 4.99,
+      title: 'Appetizers'
+    },
+    {
+      name: 'Cesear',
+      description: 'yummy homemade fries',
+      price: 1.99,
+      title: 'Salads'
+    },
+    {
+      name: 'Potato Salad',
+      description: 'served with pita bread',
+      price: 4.99,
+      title: 'Salads'
+    },
+    {
+      name: 'Mexican',
+      description: 'with celery sticks and blue cheese sauce',
+      price: 5.99,
+      title: 'Salads'
+    },
+    {
+      name: 'Magical',
+      description: 'beer battered and served with chipotle mayo',
+      price: 4.99,
+      title: 'Salads'
+    },
+    {
+      name: 'Beef',
+      description: 'yummy homemade fries',
+      price: 1.99,
+      title: 'Tacos'
+    },
+    {
+      name: 'Chicken',
+      description: 'served with pita bread',
+      price: 4.99,
+      title: 'Tacos'
+    },
+    {
+      name: 'Lengua',
+      description: 'with celery sticks and blue cheese sauce',
+      price: 5.99,
+      title: 'Tacos'
+    },
+    {
+      name: 'Carnitas',
+      description: 'beer battered and served with chipotle mayo',
+      price: 4.99,
+      title: 'Tacos'
+    }
+  ]
+};
---
+++ server/models/menuItems.js
@@ -0,0 +1,20 @@
+const { db } = require('../../database/index.js');
+
+module.exports.newItem = (item) => {
+  return db.one(
+    'INSERT INTO menu_items\
+    (name, course, food_category, price, item_description)\
+    VALUES (${name}, ${course}, ${price}, ${item_description})\
+    RETURNING item_id',
+    item);
+};
+
+module.exports.update = (vendor_id, column, updatedValue) => {
+  return db.query('UPDATE users SET $1~ = $2 WHERE user_id = $3 RETURNING *', [column, updatedValue, vendor_id]);
+};
+
+// permit is at index 17 in the array
+// food items is index 19
+// EXAMPLE RETURN OBJ
+// { permit_number: '17MFF-0179',
+//   food_category: 'Cold Truck: Pre-packaged Sandwiches: Various Beverages: Salads: Snacks' }
