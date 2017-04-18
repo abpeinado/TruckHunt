ab7019922201af2843431bf9fb8b66be8e645015
burt23 <mail4mdb@gmail.com>
2017-04-17 19:08:36 -0700

    Deleted old login file

--- login.js
+++
@@ -1,6 +0,0 @@
-const { db } = require('../../database/index.js');
-
-// TODO: finish writing checkUsername && login functions
-// module.exports.checkUsername = ( user_info ) => {
-//   return db.query('SELECT * FROM vendors where ')
-// }
