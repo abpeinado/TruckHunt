850088447d039ad146bff0285edbade4c7d04bec
Matt Schmitz <mattschmitz9@gmail.com>
2017-04-17 20:24:29 -0700

    (refact) add comments outlining expected server routes and interfaces

--- server/server.js
+++ server/server.js
@@ -22,9 +22,13 @@ Use the POST to '/search' as an example.
 **
 **/
 
+/**
+GET /search
+Query: lat, long, time and optionally radius
+response: [trucks] (include lat/long, menu data, etc)
+**/
+// ******REFACTOR THESE ROUTES INTO THE ABOVE********
 app.post('/search', requestHandler.search);
-
-app.post('/truckSignup', requestHandler.truckSignup);
 
 app.get('/truckLocations', (req, res) => {
   res.send(truckLocs.truckLocs.trucks);
@@ -37,10 +41,47 @@ app.get('/trucklist', (req, res) => {
 app.get('/truckInfo', (req, res) => {
   res.send(truckData.truckList.trucks[0]);
 });
+// ************************************************
+
+
+/**
+POST /vendorSignup
+body: {email, password, etc}
+**/
+// ******REFACTOR This ROUTE INTO THE ABOVE********
+app.post('/truckSignup', requestHandler.truckSignup);
+// ************************************************
+
+/**
+POST /customerSignup
+body: {email, password, etc}
+**/
+
+/**
+POST /vendorLogin
+body: {email, password}
+**/
+
+/**
+POST /customerLogin
+body: {email, password}
+**/
+
+/**
+POST /checkout
+body: {stripe ID, menu item id's, vendor id}
+response: order number
+**/
 
 app.get('/vendorIncomingOrder', (req, res) => {
   res.send(orderingData.VendorOrders.order);
+  // response: [orders] -- will include all orderes with status of unfulfilled (0)
 });
+
+/**
+POST /orderStatus
+body: {order number, new status}
+**/
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/public/index.html'));
