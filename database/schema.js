f651f95a8edaae53cf1540b1f205eeb40f9cd2d3
Benicio Peinado <BenicioPeinado@gmail.com>
2017-04-18 21:44:17 -0700

    (bug) match schemas

--- database/schema.js
+++ database/schema.js
@@ -25,7 +25,7 @@ module.exports = (db) => {
   .then(() => {
     return db.query('CREATE TABLE IF NOT EXISTS customers(\
       customer_id SERIAL PRIMARY KEY,\
-      email VARCHAR(50) NOT NULL UNIQUE,\
+      email VARCHAR(50) UNIQUE,\
       stripe_id INT,\
       username VARCHAR(30) NOT NULL,\
       password VARCHAR(255) NOT NULL,\
