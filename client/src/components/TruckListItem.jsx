5bca8dbf10c4039bb3985379811554fcb9e1c4d0
samzoll22 <sam.zoll22@gmail.com>
2017-04-20 00:53:38 -0700

    (feat) added randomized placeholder images to trucklist

--- client/src/components/TruckListItem.jsx
+++ client/src/components/TruckListItem.jsx
@@ -8,11 +8,13 @@ class TruckListItem extends Component {
 
   render() {
     const info = this.props.restaurant;
+    const random = (Math.floor(Math.random() * 4) + 1);
+    const image = `https://s3-us-west-1.amazonaws.com/zollstorage/MapMarker(large)V${random}.png`;
 
     return (
       <ListGroupItem className="trucklist-group" >
         <Col xs={12} sm={4} style={{ textAlign: 'center' }}>
-          <img src={info.image} style={{ maxHeight: '90px' }} alt={`${info.vendor_name}-img`} />
+          <img src={image} style={{ maxHeight: '90px' }} alt={`${info.vendor_name}-img`} />
         </Col>
         <Col xs={12} sm={8}>
           <Col xs={9} sm={8}>
