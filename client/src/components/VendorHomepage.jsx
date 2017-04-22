ca291321bfc4bacef761ecbeea8306d96694bf29
burt23 <mail4mdb@gmail.com>
2017-04-22 11:11:23 -0700

    Saving progress per new PR rebase, working on integrating stripe API for vendors

--- client/src/components/VendorHomepage.jsx
+++ client/src/components/VendorHomepage.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import VendorHeader from './VendorHeader.jsx';
 import VendorCurrentOrders from './VendorCurrentOrders.jsx';
+import { Link } from 'react-router-dom';
 
 const VendorHomepage = () => (
   <div>
     <VendorHeader />
     <VendorCurrentOrders />
+
+
+    <button>hello</button>
   </div>
 );
 
