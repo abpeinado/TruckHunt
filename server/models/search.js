7d9d9674d12ed6f198309c08b1e8fbaf747ad3ae
Matt Schmitz <mattschmitz@users.noreply.github.com>
2017-04-24 12:29:25 -0700

    (bug) add in food category

--- server/models/search.js
+++ server/models/search.js
@@ -5,5 +5,5 @@ const { db } = require('../../database/index.js');
 // };
 
 module.exports.scheduleData = () => {
-  return db.query('SELECT v.vendor_id, v.vendor_name, v.phone_number, s.day_of_week, s.start_time, s.end_time, s.coordinates, r.rating from vendors as v inner join schedules as s on v.vendor_id = s.vendor_id inner join reviews as r on v.vendor_id = r.vendor_id;');
+  return db.query('SELECT v.vendor_id, v.vendor_name, v.food_category, v.phone_number, s.day_of_week, s.start_time, s.end_time, s.coordinates, r.rating from vendors as v inner join schedules as s on v.vendor_id = s.vendor_id inner join reviews as r on v.vendor_id = r.vendor_id;');
 };
