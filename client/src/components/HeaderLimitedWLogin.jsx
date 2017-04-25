6f67a0f84a8e4cbe47319ca9aaef36af8fe05912
samzoll22 <sam.zoll22@gmail.com>
2017-04-25 10:55:19 -0700

    (feat) adjust header for menu page, search removed

---
+++ client/src/components/HeaderLimitedWLogin.jsx
@@ -0,0 +1,51 @@
+/* eslint-disable operator-assignment */
+
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Menu } from 'semantic-ui-react';
+import Logo from './Logo.jsx';
+
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  handleLogout() {
+    console.log('inside handleLogout');
+    this.props.setUserID(0);
+  }
+
+  render() {
+    return (
+      <Menu size="large" borderless>
+        <Menu.Item>
+          <Logo />
+        </Menu.Item>
+        <Menu.Menu position="right">
+          <Menu.Item >
+            {this.props.setUserID === 0 ?
+              (<Link to="/auth" className="NavBarFoodTruck">
+                  Login
+              </Link>) :
+             (<Link to="/" onClick={this.handleLogout} className="NavBarFoodTruck">
+                  Logout
+              </Link>)
+            }
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    setUserID: state.setUserID
+  };
+};
+
+
+export default connect(mapStateToProps)(Header);
