c3ce5f55784e62ea7ecc96475b1533859709be8e
samzoll22 <sam.zoll22@gmail.com>
2017-04-25 00:15:30 -0700

    (feat) implement structure for past orders

--- client/src/components/VendorPastOrders.jsx
+++ client/src/components/VendorPastOrders.jsx
@@ -1,20 +1,65 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Col } from 'react-bootstrap';
+import { vendorIncomingOrderFetchData } from '../actions/vendorIncomingOrderActions.js';
 import VendorHeader from './VendorHeader.jsx';
+import IncomingItem from './VendorCurrentOrderIncomingItem.jsx';
 
 class VendorPastOrders extends Component {
 
+  componentDidMount() {
+    this.props.FetchVendorOrders('/vendorIncomingOrders', 74);
+  }
+
   render() {
+    if (this.props.vendorIncomingOrderHasErrored) {
+      return (
+        <div>
+          <VendorHeader />
+          <p>Oops! There was an error loading the incoming order</p>;
+        </div>
+      );
+    }
+    if (this.props.vendorIncomingOrderIsLoading) {
+      return (
+        <div>
+          <VendorHeader />
+          <p>Loading Incoming Orders…</p>;
+        </div>
+      );
+    }
     return (
       <div>
         <VendorHeader />
-        <div>
-          <h2>
-            Past Orders Coming Soon!
-          </h2>
-        </div>
+        <Col xs={12} >
+          <h2 style={{ textAlign: 'center' }}>COMPLETED ORDERS</h2>
+          {this.props.vendorIncomingOrder.map((item, i) => {
+            if (item.order_status) {
+              return <IncomingItem incomingOrder={item} key={i} />;
+            }
+          }
+          )}
+        </Col>
       </div>
     );
   }
 }
 
-export default VendorPastOrders;
+const mapStateToProps = (state) => {
+  return {
+    vendorIncomingOrder: state.vendorIncomingOrder,
+    vendorIncomingOrderHasErrored: state.vendorIncomingOrderHasErrored,
+    vendorIncomingOrderIsLoading: state.vendorIncomingOrderIsLoading,
+    setUserID: state.setUserID
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    FetchVendorOrders: (url, id) => dispatch(vendorIncomingOrderFetchData(url, id))
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(VendorPastOrders);
+
+// export default VendorPastOrders;
