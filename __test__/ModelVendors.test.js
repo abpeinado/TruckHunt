4fd436864ba6b859c4cdf09a35edea1e8ccf95d3
Benicio Peinado <BenicioPeinado@gmail.com>
2017-04-25 19:52:12 -0700

    (test) test menu items model

--- __test__/ModelVendors.test.js
+++ __test__/ModelVendors.test.js
@@ -1,27 +1,12 @@
-require('dotenv').config();
-
-process.env.DATABASE_URL = 'postgres://@localhost:5432/toadstest';
-
 const Vendor = require('../server/models/vendors.js');
-const { db, loadDb } = require('../database/index.js');
+const { db } = require('../database/index.js');
+// --runInBand
 
 const testVendor = {
   vendor_name: 'BH & MT LLC',
   permit_number: '17MFF-0125',
   food_category: 'Cold Truck: Breakfast: Sandwiches: Salads: Pre-Packaged Snacks: Beverages'
 };
-
-const resetDb = () => (
-  db.none('TRUNCATE vendors RESTART IDENTITY CASCADE')
-);
-
-beforeAll(() => (
-  loadDb(db)
-));
-
-afterAll(() => (
-  resetDb()
-));
 
 describe('vendors table', () => {
   it('should have a vendors table', (done) => {
@@ -35,33 +20,27 @@ describe('vendors table', () => {
       });
   });
 
-  it('should add a new vendor into the table', () => {
-    return Vendor.newVendor(testVendor)
-      .then((vendor) => {
-        expect(vendor.vendor_id).toBeDefined();
-        expect(vendor.vendor_name).toEqual(testVendor.vendor_name);
-      });
-  });
-
   it('should find vendor permits and IDs', () => {
     return Vendor.findVendorPermitsAndIds()
-      .then((vendor) => {
-        expect(vendor[0].vendor_id).toEqual(testVendor.vendor_name);
-        expect(vendor[0].vendor_name).toEqual(testVendor.vendor_name);
+      .then((vendors) => {
+        expect(vendors.length > 1).toEqual(true);
+        expect(vendors[0].permit_number).toBeDefined();
+        expect(vendors[0].vendor_id).toBeDefined();
       });
   });
 
   it('should find vendor IDs', () => {
     return Vendor.findVendorIds()
-      .then((vendor) => {
-        expect(vendor[0].vendor_id).toEqual(testVendor.vendor_name);
+      .then((vendors) => {
+        expect(vendors.length > 0).toEqual(true);
+        expect(vendors[0].vendor_id).toBeDefined();
       });
   });
 
   it('should find vendor by permit number', () => {
     return Vendor.findVendorIdByPermitNumber(testVendor.permit_number)
       .then((vendor) => {
-        expect(vendor.permit_number).toEqual(testVendor.permit_number);
+        expect(vendor.vendor_id).toBeDefined();
       });
   });
 });
