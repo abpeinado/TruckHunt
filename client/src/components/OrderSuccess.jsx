83d34e316e8c1aa817211a072f566fe8dbb8cc7e
samzoll22 <sam.zoll22@gmail.com>
2017-04-27 14:56:58 -0700

    (fix) fix for linter

--- client/src/components/OrderSuccess.jsx
+++ client/src/components/OrderSuccess.jsx
@@ -1,4 +1,6 @@
 /* eslint-disable no-unused-expressions */
+/* eslint-disable prefer-const */
+/* eslint-disable prefer-template */
 
 import React from 'react';
 import { Grid, Segment, Header, Icon, Label } from 'semantic-ui-react';
@@ -19,31 +21,29 @@ class OrderSuccess extends React.Component {
       formattedTime = endtime + ' AM';
     }
 
-
-    console.log('ORDER SUCCESS TRUCK:', this.props.truckSelected.vendor_name)
     return (
       <div>
         <HeaderBar />
         <Grid className="gridWrapper" textAlign="center">
           <Grid.Column className="gridLeftWrapper truck-info-left-pane" width={12}>
-            <Segment inverted color="green" className="animated fadeIn menuHeader" style={{ 'minHeight': '12em' }}>
+            <Segment inverted color="green" className="animated fadeIn menuHeader" style={{ minHeight: '12em' }}>
               <Header as="h1" icon textAlign="center">
-                <Header.Content style={{ 'paddingTop': '1.2em' }} className="animated tada" >
-                  <Icon.Group color="grey"  >
+                <Header.Content style={{ paddingTop: '1.2em' }} className="animated tada" >
+                  <Icon.Group color="grey" >
                     <Icon size="large" name="checkmark" />
                   </Icon.Group>
-                  <div style={{ paddingBottom: "2em" }}>
+                  <div style={{ paddingBottom: '2em' }}>
                   Order Complete!
                   </div>
                 </Header.Content>
               </Header>
               <Header as="h2" textAlign="center" >
-                <Header.Content style={{ 'paddingBottom': '1.2em' }}>
+                <Header.Content style={{ paddingBottom: '1.2em' }}>
                   <div>
-                  Your order from <Label color='yellow' size="huge" horizontal>{this.props.truckSelected.vendor_name}</Label> will be ready soon!
+                  Your order from <Label color="yellow" size="huge" horizontal>{this.props.truckSelected.vendor_name}</Label> will be ready soon!
                   </div>
 
-                  Don't forget to pickup your order by <Label color='yellow' horizontal>{formattedTime}</Label>!
+                  Don't forget to pickup your order by <Label color="yellow" horizontal>{formattedTime}</Label>!
                 </Header.Content>
               </Header>
             </Segment>
