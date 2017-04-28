fd96d84a52597f51705d4aaaf994eb13df80e0a9
Benicio Peinado <BenicioPeinado@gmail.com>
2017-04-27 20:20:28 -0700

    Update VendorHeader.jsx

--- client/src/components/VendorHeader.jsx
+++ client/src/components/VendorHeader.jsx
@@ -63,47 +63,4 @@ const mapStateToProps = (state) => {
   };
 };
 
-
 export default connect(mapStateToProps, null)(VendorHeader);
-
-// import React from 'react';
-// import { Navbar, NavItem, Nav } from 'react-bootstrap';
-// import { Link } from 'react-router-dom';
-// import Logo from './Logo.jsx';
-
-// const VendorHeader = () => (
-//   <div className="NavbarStyled" >
-//     <Navbar>
-//       <Navbar.Header>
-//         <Logo />
-//         <Navbar.Toggle />
-//       </Navbar.Header>
-//       <Nav bsStyle="pills" className="NavbarVendor" pullRight navbar>
-//         <NavItem eventKey={1} title="CurrentOrders">
-//           <Link to="/vendor">
-//             Current Orders
-//           </Link>
-//         </NavItem>
-//         <NavItem eventKey={2} title="MenuSelection">
-//           <Link to="/menuSelection">
-//           Menu Selection
-//           </Link>
-//         </NavItem>
-//         <NavItem eventKey={3} title="PastOrders">
-//           <Link to="pastOrders">
-//             Past Orders
-//           </Link>
-//         </NavItem>
-//         <NavItem eventKey={4} title="Analytics">
-//           <Link to="/analytics">
-//             Analytics
-//           </Link>
-//         </NavItem>
-//       </Nav>
-//     </Navbar>
-//   </div>
-// );
-
-// export default VendorHeader;
-
-// <Nav bsStyle="pills" activeKey={1} className="NavbarVendor" pullRight navbar>
