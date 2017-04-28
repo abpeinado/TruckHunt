d9cbe2cd1fb4ffb67445995548cfbe28bb2f418e
samzoll22 <sam.zoll22@gmail.com>
2017-04-27 18:21:37 -0700

    (feat) improved vendor UI

--- client/src/components/VendorAnalytics.jsx
+++ client/src/components/VendorAnalytics.jsx
@@ -10,13 +10,14 @@ import utils from '../utils.js';
 class VendorAnalytics extends Component {
 
   render() {
-    const data = this.props.vendorIncomingOrder
+    const data = this.props.vendorIncomingOrder;
     let totalRev = 0;
     let totalOrders = 0;
     data.map((item) => {
       totalOrders += 1;
       totalRev += item.price_total;
     })
+    let averageOrder = utils.formatCentsToDollars(totalRev) / totalOrders;
 
     return (
       <div>
@@ -28,7 +29,7 @@ class VendorAnalytics extends Component {
           </h1>
         </Row>
         <Row>
-          <Message style={{ height: "8vh" }}>
+          <Message style={{ height: "10em" }}>
             <Col md={6} style={{textAlign:"center", "padding-bottom": "4em"}}>
               <Statistic size="huge">
                 <Statistic.Value>${utils.formatCentsToDollars(totalRev)}</Statistic.Value>
@@ -43,7 +44,7 @@ class VendorAnalytics extends Component {
             </Col>
           </Message>
         </Row>
-        <Row>
+        <Row style={{"padding-bottom": "4em"}} >
           <Col md={6}>
             <Message>
               <AreaChart
@@ -52,11 +53,11 @@ class VendorAnalytics extends Component {
                 data={data}
                 margin={{top: 10, right: 30, left: 0, bottom: 0}}
               >
-                <Area type='monotone' dataKey='price_total' stroke='#8884d8' fill='#8884d8' />
+                <Area type='monotone' dataKey='price_total' stroke='#FFA500' fill='#FFA500' />
                 <Tooltip/>
                 <CartesianGrid strokeDasharray="3 3" />
               </AreaChart>
-              <h2 style={{textAlign:"center"}}>
+              <h2 style={{ textAlign:"center" }}>
                 Average Price
               </h2>
             </Message>
@@ -70,21 +71,29 @@ class VendorAnalytics extends Component {
                 <CartesianGrid strokeDasharray="3 3" />
                 <Tooltip />
                 <Legend />
-                <Bar dataKey="order_time" fill="#8884d8" />
+                <Bar dataKey="order_time" fill="#FFA500" />
               </BarChart>
-
               <h2 style={{textAlign:"center"}}>
                 Average Order Delivery Status
               </h2>
             </Message>
           </Col>
         </Row>
+         <Row>
+          <Message style={{ height: "10em" }}>
+            <Col md={12} style={{textAlign:"center", "padding-bottom": "4em"}}>
+              <Statistic size="huge">
+                <Statistic.Value>${averageOrder}</Statistic.Value>
+                <Statistic.Label>Average Price Per Order</Statistic.Label>
+              </Statistic>
+            </Col>
+          </Message>
+        </Row>
         </Grid>
       </div>
     );
   }
 }
-
 
 const mapStateToProps = (state) => {
   return {
